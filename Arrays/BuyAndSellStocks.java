090fc309e642bfe8d3631e1acff955fd5dcea7c8
Mithilesh-salunkhe <144833325+Mithilesh-salunkhe@users.noreply.github.com>
2025-03-27 12:02:18 +0530

    added comments

--- Arrays/BuyAndSellStocks.java
+++ Arrays/BuyAndSellStocks.java
@@ -5,16 +5,16 @@ public class BuyAndSellStocks {
         int maxProfit = 0;
 
         for(int i = 0 ; i<prices.length ; i++){
-            if(buyPrice < prices[i]){
+            if(buyPrice < prices[i]){ //profit
                 int profit = prices[i] - buyPrice;
                 maxProfit = Math.max(maxProfit , profit);
-            }else{
+            }else{ //loss
                 buyPrice = prices[i];
             }
         }
         return maxProfit;
     }
-    public static void main(String args[]){
+    public static void main(String args[]){ //T.C -->O(n)
         int arr[] = {7,1,5,3,6,4};
         System.out.println(buyAndSellStocks(arr));
 
