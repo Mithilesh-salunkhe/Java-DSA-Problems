c513f6b691a4dbde2615f8bafe5e3ead2bc57885
Mithilesh-salunkhe <144833325+Mithilesh-salunkhe@users.noreply.github.com>
2025-06-24 08:06:53 +0530

    Add files via upload

---
+++ Strings/string_Compression.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class string_Compression {
+
+    public static String compress(String str){
+        StringBuilder sb = new StringBuilder(" ");
+
+        for(int i = 0 ; i < str.length(); i++){
+            Integer count = 1;
+
+            while(i < str.length() -1 && str.charAt(i) == str.charAt(i+1)){
+                count ++;
+                i++;
+            }
+
+            sb.append(str.charAt(i));
+            if(count > 1){
+                sb.append(count.toString());
+            }
+        }
+        return sb.toString();
+    }
+    
+ 
+    public static void main(String args[]){
+        String str = "aaabbcccdd";
+        System.out.println(compress(str)); //T.C --> O(n) bcz as we are using two nested loop but traversing throung whole string 1 time only ..
+    }
+}   
